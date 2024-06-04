45737a798b2376a778109308f5fc64def4d7762f
amruth92 <165199381+amruth92@users.noreply.github.com>
2024-06-04 13:21:28 +0100

    Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,11 @@ const Navbar = () => {
 		<div className="my-5 border bg-white shadow-sm">
 			<div className="container mx-auto py-5">
 				<p className="text-3xl font-bold text-center">
-					Guess The Song Title
+					Ravle
+				</p>
+				<br />
+				<p ClassName="text-2xl text-center"> 
+				Something new is coming on the 19th of July - but can you guess what it is called?
 				</p>
 			</div>
 		</div>
