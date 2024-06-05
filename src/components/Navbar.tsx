a6654e68aab7fd7c08a1f6a7bdf1f974e724af2a
amruth92 <165199381+amruth92@users.noreply.github.com>
2024-06-05 08:24:01 +0100

    Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,17 +3,18 @@ import React from "react";
 const Navbar = () => {
 	return (
 		<div className="my-5 border bg-white shadow-sm">
-			<div className="container mx-auto py-5">
-				<div className="flex justify-center items-center">
+			<div className="flex justify-center items-center">
     <img src="https://ravi.am/wp-content/uploads/2024/04/NewLogoDarkNoBack.png" alt="Ravi Amruth Logo">
-</div>
-				<p className="text-3xl font-bold text-center">
+
+				<div className="container mx-auto py-5">
+					<p className="text-3xl font-bold text-center">
 					Rav-le
-				</p>
-				<p className="text-1xl text-center"> 
-				Something new is coming on the 19th of July - but can you guess what it is called?
-				</p>
-			</div>
+					</p>
+					<p className="text-1xl text-center"> 
+					Something new is coming on the 19th of July - but can you guess what it is called?
+					</p>
+				</div>
+    			</div>
 		</div>
 	);
 };
