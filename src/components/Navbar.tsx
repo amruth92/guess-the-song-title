256f862319f61a5155f91894b305fd38eb8098b7
amruth92 <165199381+amruth92@users.noreply.github.com>
2024-06-05 21:20:23 +0100

    Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,42 +1,18 @@
 import React from "react";
 
 const Navbar = () => {
-  const [showInstructions, setShowInstructions] = React.useState(false);
-
-  const toggleInstructions = () => {
-    setShowInstructions(!showInstructions);
-  };
-
-  const InstructionsPopup = () => {
-    if (typeof window === 'undefined') return null; // Don't render on server
-
-    return (
-      <>
-        {showInstructions && (
-          <div className="instructions-popup">
-            <div className="instructions-content">
-              <h2>Game Instructions</h2>
-              <p>Write your code here...</p>
-              <button onClick={toggleInstructions}>Close</button>
-            </div>
-          </div>
-        )}
-      </>
-    );
-  };
-
-  return (
-    <div className="my-5 border bg-white shadow-sm">
-      <div className="container mx-auto py-5">
-        <div className="justify-center items-center text-center">
-          <img src="http://ravi.am/wp-content/uploads/2024/06/ravle-logo-1.jpg" alt="Ravi Amruth Logo" className="mx-auto" />
-        </div>
-        <p className="text-1xl text-center">Something new is coming on the 19th of July - but can you guess what it is called?</p>
-        <button onClick={toggleInstructions}>Show Instructions</button>
-      </div>
-      <InstructionsPopup />
-    </div>
-  );
+	return (
+		<div className="my-5 border bg-white shadow-sm">
+			<div className="container mx-auto py-5">
+				<div className="justify-center items-center text-center">
+    				<img src="http://ravi.am/wp-content/uploads/2024/06/ravle-logo-1.jpg" alt="Ravi Amruth Logo" className="mx-auto">
+				</img> </div>
+			<p className="text-1xl text-center"> 
+			Something new is coming on the 19th of July - but can you guess what it is called?
+			</p>
+    			</div>
+		</div>
+	);
 };
 
 export default Navbar;
