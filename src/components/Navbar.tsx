dff1c2f84158e348a9d932159fc5abc35aab95ca
amruth92 <165199381+amruth92@users.noreply.github.com>
2024-06-05 08:10:35 +0100

    Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,11 +4,11 @@ const Navbar = () => {
 	return (
 		<div className="my-5 border bg-white shadow-sm">
 			<div className="container mx-auto py-5">
+				<img src="https://ravi.am/wp-content/uploads/2024/04/NewLogoDarkNoBack.png"></img>
 				<p className="text-3xl font-bold text-center">
 					Rav-le
 				</p>
-				<br />
-				<p className="text-2xl text-center"> 
+				<p className="text-1xl text-center"> 
 				Something new is coming on the 19th of July - but can you guess what it is called?
 				</p>
 			</div>
