ef4a3e6787b0f92e7a83e553bf83102f45e43791
amruth92 <165199381+amruth92@users.noreply.github.com>
2024-06-05 08:18:51 +0100

    Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,9 @@ const Navbar = () => {
 	return (
 		<div className="my-5 border bg-white shadow-sm">
 			<div className="container mx-auto py-5">
-				<img src="https://ravi.am/wp-content/uploads/2024/04/NewLogoDarkNoBack.png"></img>
+				<div class="flex justify-center items-center">
+    <img src="https://ravi.am/wp-content/uploads/2024/04/NewLogoDarkNoBack.png" alt="Ravi Amruth Logo">
+</div>
 				<p className="text-3xl font-bold text-center">
 					Rav-le
 				</p>
