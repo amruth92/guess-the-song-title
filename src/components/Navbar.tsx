e37b674915d81933dd6fcda61b3b58c7c4c12262
amruth92 <165199381+amruth92@users.noreply.github.com>
2024-06-05 10:44:00 +0100

    Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,10 +4,9 @@ const Navbar = () => {
 	return (
 		<div className="my-5 border bg-white shadow-sm">
 			<div className="container mx-auto py-5">
-				<Image
-  				style={{ alignSelf: 'center' }}
-  				source={require('http://ravi.am/wp-content/uploads/2024/05/NewLogo-e1717571719812.jpg')}
-				/>
+				<div className="justify-center items-center text-center">
+    				<img src="http://ravi.am/wp-content/uploads/2024/05/NewLogo-e1717571719812.jpg" alt="Ravi Amruth Logo">
+				</img> </div>
 			<p className="text-3xl font-bold text-center">
 			Rav-le
 			</p>
