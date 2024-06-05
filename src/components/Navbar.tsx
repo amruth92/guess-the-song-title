0080779511dbe2bc849f1745a838614bc6d910da
amruth92 <165199381+amruth92@users.noreply.github.com>
2024-06-05 20:57:18 +0100

    Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import InstructionsPopup from "./InstructionsPopup";
 
 const Navbar = () => {
-	return (
-		<div className="my-5 border bg-white shadow-sm">
-			<div className="container mx-auto py-5">
-				<div className="justify-center items-center text-center">
-    				<img src="http://ravi.am/wp-content/uploads/2024/06/ravle-logo-1.jpg" alt="Ravi Amruth Logo" className="mx-auto">
-				</img> </div>
-			<p className="text-1xl text-center"> 
-			Something new is coming on the 19th of July - but can you guess what it is called?
-			</p>
-    			</div>
-		</div>
-	);
+  const [showInstructions, setShowInstructions] = useState(false);
+
+  const toggleInstructions = () => {
+    setShowInstructions(!showInstructions);
+  };
+
+  return (
+    <div className="my-5 border bg-white shadow-sm">
+      <div className="container mx-auto py-5">
+        <div className="justify-center items-center text-center">
+          <img src="http://ravi.am/wp-content/uploads/2024/06/ravle-logo-1.jpg" alt="Ravi Amruth Logo" className="mx-auto" />
+        </div>
+        <p className="text-1xl text-center">Something new is coming on the 19th of July - but can you guess what it is called?</p>
+        <button onClick={toggleInstructions}>Show Instructions</button>
+      </div>
+      {showInstructions && <InstructionsPopup onClose={toggleInstructions} />}
+    </div>
+  );
 };
 
 export default Navbar;
