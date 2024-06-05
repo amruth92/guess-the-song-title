405d52afcf4d1ab463a308f3c97a3529786a70e9
amruth92 <165199381+amruth92@users.noreply.github.com>
2024-06-05 19:24:24 +0100

    Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,7 @@ const Navbar = () => {
 		<div className="my-5 border bg-white shadow-sm">
 			<div className="container mx-auto py-5">
 				<div className="justify-center items-center text-center">
-    				<img src="http://ravi.am/wp-content/uploads/2024/06/ravle-logo-e1717604647281.jpg" alt="Ravi Amruth Logo" className="mx-auto">
+    				<img src="http://ravi.am/wp-content/uploads/2024/06/ravle-logo-1.jpg" alt="Ravi Amruth Logo" className="mx-auto">
 				</img> </div>
 			<p className="text-1xl text-center"> 
 			Something new is coming on the 19th of July - but can you guess what it is called?
